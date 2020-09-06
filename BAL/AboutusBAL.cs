394904d35a78ced41ff62bb39ebe510afb0f8f43
Mansi2523 <61613688+Mansi2523@users.noreply.github.com>
2020-09-06 21:54:58 +1200

    Project files and solution
    Project files added

---
+++ BAL/AboutusBAL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PAL;
+using System.Data.SqlClient;
+using DAL;
+using System.Data;
+
+namespace BAL
+{
+    public class AboutusBAL:AboutusPAL
+    {
+        DataTable dt = new DataTable();
+        DataSet ds = new DataSet();
+
+        public DataSet ManageAboutUS()
+        {
+            SqlParameter[] Params = new SqlParameter[3];
+            Params[0] = new SqlParameter("@AboutusShortDesc", AboutusShortDesc);
+            Params[1] = new SqlParameter("@AboutusDescription", AboutusDescription);
+            Params[2] = new SqlParameter("@Action", Action);
+            return SqlHelper.ExecuteDataset(CommandType.StoredProcedure, "sp_ManageAboutUS", Params);
+        }
+    }
+}
