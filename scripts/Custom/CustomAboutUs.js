e7ebcfa8c20179e3d330c15a9d600b6fead634f1
Mansi2523 <61613688+Mansi2523@users.noreply.github.com>
2020-09-06 22:13:22 +1200

    Add files via upload
    js

---
+++ scripts/Custom/CustomAboutUs.js
@@ -0,0 +1,28 @@
+﻿//Int JS
+$(document).ready(function () {    
+
+    //Get Short  Description FrontEnd     
+    $.ajax({
+        type: "POST",
+        url: "Default.aspx/GetShortDesc",
+        datatype: "json",
+        contentType: 'application/json; charset=utf-8',
+        success: function (response) {
+            var Data = JSON.parse(response.d);
+            $("#aboutussd").html(Data);
+        }
+    });
+
+
+    //Get Short  Description FrontEnd     
+    $.ajax({
+        type: "POST",
+        url: "AboutUs.aspx/GetDesc",
+        datatype: "json",
+        contentType: 'application/json; charset=utf-8',
+        success: function (response) {
+            var Data = JSON.parse(response.d);
+            $("#aboutusld").html(Data);
+        }
+    });
+});
